74c5b1dda7982e4bc03ab7472b58281e2942fd96
Genna Walsh <gennawalsh@gmail.com>
2018-11-27 18:01:46 +0000

    add Park class and tests

--- src/main/java/Attractions/Park.java
+++ src/main/java/Attractions/Park.java
@@ -1,4 +1,11 @@
 package Attractions;
 
-public class Park {
+import Main.Attraction;
+
+public class Park extends Attraction {
+
+    public Park(String name) {
+        super(name);
+    }
+
 }
--- src/test/java/ParkTest.java
+++ src/test/java/ParkTest.java
@@ -1,2 +1,28 @@
+import Attractions.Park;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ParkTest {
+
+    Park park;
+
+    @Before
+    public void setup() {
+        park = new Park("Parky");
+    }
+
+    //    Inherited things tests
+
+    @Test
+    public void canGetName() {
+        assertEquals("Parky", park.getName());
+    }
+
+    @Test
+    public void canGetRating() {
+        assertEquals(10, park.getRating());
+    }
+
 }
