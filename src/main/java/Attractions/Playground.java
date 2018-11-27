0c1a60425aeaade5a94099c7ffd01273f0f2b3e3
Genna Walsh <gennawalsh@gmail.com>
2018-11-27 18:09:44 +0000

    add Playground class and tests

--- src/main/java/Attractions/Playground.java
+++ src/main/java/Attractions/Playground.java
@@ -1,4 +1,20 @@
 package Attractions;
 
-public class Playground {
+import Interfaces.ISecurity;
+import Main.Attraction;
+import Main.Visitor;
+
+public class Playground extends Attraction implements ISecurity {
+
+    public Playground(String name) {
+        super(name);
+    }
+
+    public boolean isAllowedTo(Visitor visitor) {
+        if (visitor.getAge() <= 15) {
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/test/java/PlaygroundTest.java
+++ src/test/java/PlaygroundTest.java
@@ -1,2 +1,44 @@
+import Attractions.Playground;
+import Main.Visitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaygroundTest {
+
+    Playground playground;
+    Visitor visitor;
+
+    @Before
+    public void setup() {
+        playground = new Playground("Groundy");
+    }
+
+    //    Inherited things tests
+
+    @Test
+    public void canGetName() {
+        assertEquals("Groundy", playground.getName());
+    }
+
+    @Test
+    public void canGetRating() {
+        assertEquals(10, playground.getRating());
+    }
+
+    //    Rollercoaster tests
+
+    @Test
+    public void visitorAllowedIfMeetsRequirements() {
+        visitor = new Visitor(15, 150, 100);
+        assertEquals(true, playground.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void visitorNotAllowedIfDoesntMeetRequirements() {
+        visitor = new Visitor(16, 150, 100);
+        assertEquals(false, playground.isAllowedTo(visitor));
+    }
+
 }
