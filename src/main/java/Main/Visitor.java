bbc1b66aebb8e74e2d8e694857db5185adf5b3c8
Genna Walsh <gennawalsh@gmail.com>
2018-11-27 17:13:25 +0000

    first commit, Visitor class

---
+++ src/main/java/Main/Visitor.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class Visitor {
+
+    private int age;
+    private int height;
+    private double money;
+
+    public Visitor(int age, int height, double money) {
+        this.age = age;
+        this.height = height;
+        this.money = money;
+    }
+
+}
