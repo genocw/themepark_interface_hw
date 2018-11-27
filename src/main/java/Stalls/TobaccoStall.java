58720462f1355ec67944f4320542bac302553312
Genna Walsh <gennawalsh@gmail.com>
2018-11-27 18:40:32 +0000

    add Tobacco class and tests

--- src/main/java/Stalls/TobaccoStall.java
+++ src/main/java/Stalls/TobaccoStall.java
@@ -1,11 +1,20 @@
 package Stalls;
 
+import Interfaces.ISecurity;
 import Main.Stall;
+import Main.Visitor;
 
-public class TobaccoStall extends Stall {
+public class TobaccoStall extends Stall implements ISecurity {
 
     public TobaccoStall(String name, String ownerName, String parkingSpot) {
         super(name, ownerName, parkingSpot);
+    }
+
+    public boolean isAllowedTo(Visitor visitor) {
+        if (visitor.getAge() >= 18) {
+            return true;
+        }
+        return false;
     }
 
 }
--- src/test/java/TobaccoStallTest.java
+++ src/test/java/TobaccoStallTest.java
@@ -1,2 +1,54 @@
+import Main.Visitor;
+import Stalls.TobaccoStall;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TobaccoStallTest {
+
+    TobaccoStall tobacco;
+    Visitor visitor;
+
+    @Before
+    public void setup() {
+        tobacco = new TobaccoStall("Toby", "Miss Puff", "A3");
+    }
+
+    //    Inherited things tests
+
+    @Test
+    public void canGetName() {
+        assertEquals("Toby", tobacco.getName());
+    }
+
+    @Test
+    public void canGetRating() {
+        assertEquals(10, tobacco.getRating());
+    }
+
+    @Test
+    public void canGetOwnerName() {
+        assertEquals("Miss Puff", tobacco.getOwnerName());
+    }
+
+    @Test
+    public void canGetParkingSpot() {
+        assertEquals("A3", tobacco.getParkingSpot());
+    }
+
+//    TobaccoStall tests
+
+    @Test
+    public void visitorAllowedIfMeetsRequirements() {
+        visitor = new Visitor(18, 180, 100);
+        assertEquals(true, tobacco.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void visitorNotAllowedIfDoesntMeetRequirements() {
+        visitor = new Visitor(17, 180, 100);
+        assertEquals(false, tobacco.isAllowedTo(visitor));
+    }
+
 }
